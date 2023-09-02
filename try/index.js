481abaf8c24aa63ad5f4809d7ea1346d34dc88b8
hidden bebb <cbj4133@naver.com>
2023-09-02 19:31:18 +0900

    Changes to be committed: 	renamed:    try/img/arrow-right.png -> elice_ex/croket/img/arrow-right.png 	renamed:    try/img/click1.jpg -> elice_ex/croket/img/click1.jpg 	renamed:    try/img/click2.jpg -> elice_ex/croket/img/click2.jpg 	renamed:    try/img/click3.jpg -> elice_ex/croket/img/click3.jpg 	renamed:    try/img/image1.png -> elice_ex/croket/img/image1.png 	renamed:    try/img/image2.jpg -> elice_ex/croket/img/image2.jpg 	renamed:    try/img/image3.jpg -> elice_ex/croket/img/image3.jpg 	renamed:    try/img/image4.jpg -> elice_ex/croket/img/image4.jpg 	renamed:    try/img/image5.jpg -> elice_ex/croket/img/image5.jpg 	renamed:    try/img/team1.jpg -> elice_ex/croket/img/team1.jpg 	renamed:    try/img/team2.jpg -> elice_ex/croket/img/team2.jpg 	renamed:    try/img/team3.jpg -> elice_ex/croket/img/team3.jpg 	renamed:    try/css/index.css -> elice_ex/croket/index.css 	renamed:    try/html/index.html -> elice_ex/croket/index.html 	new file:   elice_ex/croket/index.js 	new file:   elice_ex/d/index.css 	new file:   elice_ex/d/index.html 	new file:   elice_ex/d/index.js 	deleted:    try/.vscode/launch.json 	deleted:    try/.vscode/settings.json 	deleted:    try/html/memo.html 	renamed:    try/css/memo.css -> try/index.css 	new file:   try/index.html 	modified:   try/index.js 	deleted:    try/memo.js

--- try/index.js
+++ try/index.js
@@ -1,118 +1,29 @@
+// 제시된 데이터
+var data = [
+  { name: "user1", group: 1 },
+  { name: "user2", group: 1 },
+  { name: "user3", group: 2 },
+];
 
-// 1) Scroll Navigation
-var aTags = document.querySelectorAll('header a');
-for(var i = 0; i < aTags.length; i ++) {
-    aTags[i].onclick = function(e) {
-        e.preventDefault();
-        var target = document.querySelector(this.getAttribute("href"));
+// 버튼 요소들을 가져옵니다.
+var btn1 = document.querySelector(".btn.btn1");
+var btn2 = document.querySelector(".btn.btn2");
+var btn3 = document.querySelector(".btn.btn3");
 
-        window.scrollTo({
-            'behavior': 'smooth',
-            'top': target.offsetTop
-        })
-    }
+// activateItem 함수를 정의합니다.
+function activateItem(name, group) {
+  alert(`Hi, ${name}! You are in group ${group}`);
 }
 
-// 2) Image Slider
+// 각 버튼에 이벤트 리스너를 추가하여 클릭 시 activateItem 함수를 호출합니다.
+btn1.addEventListener("click", function () {
+  activateItem(data[0].name, data[0].group);
+});
 
-var slider = document.querySelector("#slider");
-var slides = slider.querySelector(".slides");
-var slide = slides.querySelectorAll(".slide");
+btn2.addEventListener("click", function () {
+  activateItem(data[1].name, data[1].group);
+});
 
-var currentSlide = 0;
-
-setInterval(function() {
-    var from = -(1024 * currentSlide);
-    var to = from - 1024;
-    slides.animate({
-        marginLeft: [from + "px", to + "px"]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both"
-    });
-    currentSlide++;
-    if (currentSlide === (slide.length - 1)) {
-        currentSlide = 0;
-    }
-}, 3000);
-
-
-
-// 3) Tabs
-
-var links = document.querySelectorAll(".tabs-list li a")
-var items = document.querySelectorAll(".tabs-list li")
-for (var i = 0; i < links.length; i++) {
-    links[i].onclick = function(e) {
-        e.preventDefault();
-    }
-}
-
-for (var i = 0; i < items.length; i++) {
-    items[i].onclick = function() {
-        var tabId = this.querySelector("a").getAttribute("href");
-        console.log(this.classList);
-        document.querySelectorAll(".tabs-list li, .tabs div.tab").forEach(function(item) {
-            item.classList.remove("active");
-            console.log(item);
-        });
-        document.querySelector(tabId).classList.add("active");
-        this.classList.add("active");
-    }  
-} 
-
-// 4) Click Image Slider
-document.querySelector(".right-arrow").onclick = function () {
-    var currentSlide = document.querySelector("#photo .slide.active");
-    var nextSlide = currentSlide.nextElementSibling;
-    if (nextSlide === null) {
-        nextSlide = currentSlide.parentElement.firstElementChild;
-    }
-        currentSlide.animate({
-        opacity: [1, 0]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both"
-    });
-    currentSlide.classList.remove("active");
-    nextSlide.animate({
-        opacity: [0, 1]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both"
-    });
-    nextSlide.classList.add("active");
-}
-
-document.querySelector(".left-arrow").onclick = function () {
-    var currentSlide = document.querySelector("#photo .slide.active");
-    var prevSlide = currentSlide.previousElementSibling;
-    if (prevSlide === null) {
-        prevSlide = currentSlide.parentElement.lastElementChild;
-    }
-    currentSlide.animate({
-        opacity: [1, 0]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both"
-    });
-    currentSlide.classList.remove("active");
-    prevSlide.animate({
-        opacity: [0, 1]
-    }, {
-        duration: 500,
-        easing: "ease",
-        iterations: 1,
-        fill: "both"
-    });
-    prevSlide.classList.add("active");
-}
-
+btn3.addEventListener("click", function () {
+  activateItem(data[2].name, data[2].group);
+});
